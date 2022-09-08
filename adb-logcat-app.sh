10f2db2bd071d7c3cc0f2e9067554a94c4bf5ec3
Joseph Werle <joseph.werle@gmail.com>
2022-09-07 20:01:06 -0400

    refactor(): improve UX, clean up

--- adb-logcat-app.sh
+++ adb-logcat-app.sh
@@ -1,49 +1,72 @@
 #!/bin/bash
 
-VERSION=0.1.0
+declare VERSION=0.1.0
 
 usage () {
-  echo "adb-logcat-app [-hV]"
+  echo "usage: adb-logcat-app [-hV] [adb logcat options] <appid>"
   echo
-  echo "Options:"
-  echo "  -h|--help      Print this help dialogue and exit"
-  echo "  -V|--version   Print the current version and exit"
+  echo "adb logcat options:"
+  echo "  See 'adb logcat -h' "
+  echo
+  echo "options:"
+  echo "  -h,--help      Print this help dialogue and exit"
+  echo "  -V,--version   Print the current version and exit"
 }
 
 adb-logcat-app () {
-  local id opt pid retries=0
+  local id opt pid adbpid
+  local args=()
 
-  for opt in "${@}"; do
-    case "${opt}" in
+  for opt in "$@"; do
+    case "$opt" in
       -h|--help)
         usage
         return 0
         ;;
       -V|--version)
-        echo "${VERSION}"
+        echo "$VERSION"
         return 0
         ;;
 
        *)
-         id="$opt"
-         break
+         if [ -z "$id" ] && [ "${opt:0:1}" != "-" ]; then
+          id="$opt"
+        else
+          args+=("$opt")
+         fi
          ;;
     esac
   done
 
-  while [ -z "$pid" ] && (( retries++ < 32 )); do
+  if [ -z "$id" ]; then
+    echo >&2 "error: An application id must be given"
+    usage >&2
+    return 1
+  fi
+
+  while true; do
     ## Probe for application process ID
-    pid="$(adb shell ps | grep "$id" | awk '{print $2}' 2>/dev/null)"
+    pid="$(adb shell ps 2>/dev/null | grep "$id" | awk '{print $2}' | xargs echo -n)"
+    if [  -n "$pid" ]; then
+      # echo adb logcat --pid="$pid" "${args[@]}" 2>/dev/null
+      adb logcat --pid="$pid" "${args[@]}" 2>/dev/null & adbpid=$!
+      # shellcheck disable=SC2064
+      trap "kill -9 $adbpid; exit" SIGTERM SIGINT
+
+      while [ -n "$pid" ]; do
+        pid="$(adb shell ps 2>/dev/null | grep "$id" | awk '{print $2}' | xargs echo -n)"
+      done
+
+      kill -9 "$adbpid"
+      wait
+    fi
     sleep 1s
   done
-
-  adb logcat --pid="$pid"
 }
 
 if [[ ${BASH_SOURCE[0]} != "$0" ]]; then
   export -f adb-logcat-app
 else
   adb-logcat-app "${@}"
-  exit 0
+  exit $?
 fi
-
